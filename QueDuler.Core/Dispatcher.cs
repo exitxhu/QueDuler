940a22df6473fb8cfa9ce84402e6b1d08fb6b399
mohammadhadi <mohammadHadi@smartec.ir>
2022-04-06 17:52:35 +0430

    resolve issue

--- QueDuler.Core/Dispatcher.cs
+++ QueDuler.Core/Dispatcher.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Microsoft.Extensions.DependencyInjection;
+using QueDuler.Core;
 using QueDuler.Helpers;
 
 public partial class Dispatcher
@@ -19,16 +21,19 @@ public partial class Dispatcher
     {
         HashSet<IDispatchableJob> dispatchables = new();
         HashSet<ISchedulableJob> schedules = new();
+            // we need to resolve the jobs inorder to get the jobid from the implentation(in run time)
+            // but we should to resolve them again when we want to run them, other wise it wont be thread safe!
+            // specially for dispatchables
         foreach (var job in jobs.SchedulableJobs)
         {
             var j = provider.GetService(job) as ISchedulableJob
-                ?? throw new NullReferenceException($"job FullName: {job.FullName} is not injected! be sure that you used the method \"AddQueduler()\" to inject the job assembly into di container, and also chosen the right assemblies");
+                ?? throw new JobNotInjectedException(job.FullName);
             scheduler.Schedule(j);
         }
         foreach (var job in jobs.DispatchableJobs)
         {
             var j = provider.GetService(job) as IDispatchableJob
-                ?? throw new NullReferenceException($"job FullName: {job.FullName} is not injected! be sure that you used the method \"AddQueduler()\" to inject the job assembly into di container, and also chosen the right assemblies");
+                ?? throw new JobNotInjectedException(job.FullName);
             dispatchables.Add(j);
         }
         broker.OnMessageReceived += (s, a) =>
@@ -37,10 +42,16 @@ public partial class Dispatcher
             {
                 var check = DispatchableJobArgument.TryParse(a, out var arg);
                 if (check)
-                    dispatchables.SingleOrDefault(n => n.JobId == arg.JobId)?.Dispatch(arg);
+                {
+                    var j = provider.CreateScope().ServiceProvider.GetService(
+                        dispatchables.SingleOrDefault(n => n.JobId == arg.JobId)?.GetType()) as IDispatchableJob;
+                    j.Dispatch(arg);
+
+                }
             }
             catch (Exception ex)
             {
+                //Todo: something about it
                 Console.WriteLine(ex);
             }
         };
---
+++ QueDuler.Models/BaseArgument.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+
+public abstract class BaseArgument
+{
+    public abstract string ToJson();
+}
+public class DispatchableJobArgument : BaseArgument
+{
+    public DispatchableJobArgument(string jobId, object argumentObject = null)
+    {
+        JobId = jobId;
+        ArgumentObject = argumentObject;
+    }
+    public string JobId { get; }
+    public object ArgumentObject { get; }
+    public static bool TryParse(string json, out DispatchableJobArgument argument)
+    {
+        try
+        {
+            argument = JsonConvert.DeserializeObject<DispatchableJobArgument>(json);
+            return true;
+        }
+        catch (Exception)
+        {
+            argument = null;
+            return false;
+        }
+    }
+
+    public override string ToJson() => JsonConvert.SerializeObject(this);
+}
