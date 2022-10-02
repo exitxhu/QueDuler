1402259159a3098a8e86afada4127c3add6a0f13
mohammadhadi <mohammadHadi@smartec.ir>
2022-10-02 17:23:03 +0330

    log broker

--- QueDuler.Core/Dispatcher.cs
+++ QueDuler.Core/Dispatcher.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using QueDuler.Core;
 using QueDuler.Helpers;
 
@@ -9,26 +10,30 @@ public partial class Dispatcher
     private readonly IScheduler? _scheduler;
     private readonly DispatcherArg _jobs;
     private readonly IServiceProvider _provider;
-    public Dispatcher(IBroker broker, IScheduler scheduler, DispatcherArg jobs, IServiceProvider provider)
+    private readonly ILogger<Dispatcher> _logger;
+    public Dispatcher(IBroker broker, IScheduler scheduler, DispatcherArg jobs, IServiceProvider provider, ILogger<Dispatcher> logger)
     {
         _broker = broker;
         _scheduler = scheduler;
         _jobs = jobs;
         _provider = provider;
+        _logger = logger;
     }
-    public Dispatcher(IScheduler scheduler, DispatcherArg jobs, IServiceProvider provider)
+    public Dispatcher(IScheduler scheduler, DispatcherArg jobs, IServiceProvider provider, ILogger<Dispatcher> logger)
     {
         _broker = null;
         _scheduler = scheduler;
         _jobs = jobs;
         _provider = provider;
+        _logger = logger;
     }
-    public Dispatcher(IBroker broker, DispatcherArg jobs, IServiceProvider provider)
+    public Dispatcher(IBroker broker, DispatcherArg jobs, IServiceProvider provider, ILogger<Dispatcher> logger)
     {
         _scheduler = null;
         _broker = broker;
         _jobs = jobs;
         _provider = provider;
+        _logger = logger;
     }
     public void Start(CancellationToken cancellationToken)
     {
@@ -59,6 +64,8 @@ public partial class Dispatcher
             {
                 try
                 {
+                    _logger.LogWarning($"Injected OnMessageReceived received a new message received {a}");
+
                     var check = DispatchableJobArgument.TryParse(a, out var arg);
                     if (check)
                     {
@@ -70,8 +77,8 @@ public partial class Dispatcher
                 }
                 catch (Exception ex)
                 {
-                //Todo: something about it
-                    Console.WriteLine(ex);
+                    //Todo: something about it
+                    _logger.LogCritical(ex, $"Injected OnMessageReceived (queduler kafka broker) encountered an error: {ex.Message}");
                 }
             };
             Task.Run(() => _broker.StartConsumingAsyn(cancellationToken)).ConfigureAwait(false);
